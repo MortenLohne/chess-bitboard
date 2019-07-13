e3d5a02c035f270c3424b8c9a3ce4879ee475ba5
Morten Lohne <lohnemorten@gmail.com>
2019-07-13 23:23:01 +0200

    Add inline attributes everywhere to enable inlining across crate boundries

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -15,18 +15,22 @@ impl fmt::Display for Square {
 }
 
 impl Square {
+    #[inline]
     pub fn from_ints(file: u8, rank: u8) -> Self {
         debug_assert!(file < 8 && rank < 8);
         Square((rank << 3) | file)
     }
+    #[inline]
     pub fn file_rank(self) -> (u8, u8) {
         (self.file(), self.rank())
     }
     /// Files are numbered from 0 (a-file) to 7 (h-file)
+    #[inline]
     pub fn file(self) -> u8 {
         self.0 & 0b0000_0111
     }
     /// Ranks are numbered from 0 (1st rank) to 7 (8th rank)
+    #[inline]
     pub fn rank(self) -> u8 {
         self.0 >> 3
     }
@@ -51,12 +55,14 @@ pub struct BitBoard {
 
 impl ops::BitOr for BitBoard {
     type Output = BitBoard;
+    #[inline]
     fn bitor(self, rhs: BitBoard) -> BitBoard {
         BitBoard::from_u64(self.board | rhs.board)
     }
 }
 
 impl ops::BitOrAssign for BitBoard {
+    #[inline]
     fn bitor_assign(&mut self, rhs: BitBoard) {
         self.board |= rhs.board
     }
@@ -64,12 +70,14 @@ impl ops::BitOrAssign for BitBoard {
 
 impl ops::BitAnd for BitBoard {
     type Output = BitBoard;
+    #[inline]
     fn bitand(self, rhs: BitBoard) -> BitBoard {
         BitBoard::from_u64(self.board & rhs.board)
     }
 }
 
 impl ops::BitAndAssign for BitBoard {
+    #[inline]
     fn bitand_assign(&mut self, rhs: BitBoard) {
         self.board &= rhs.board
     }
@@ -77,55 +85,68 @@ impl ops::BitAndAssign for BitBoard {
 
 impl ops::Not for BitBoard {
     type Output = BitBoard;
+    #[inline]
     fn not(self) -> BitBoard {
         BitBoard::from_u64(!self.board)
     }
 }
 
 impl BitBoard {
+    #[inline]
     pub const fn empty() -> Self {
         BitBoard { board: 0 }
     }
+    #[inline]
     pub const fn from_u64(n: u64) -> Self {
         BitBoard { board: n }
     }
 
+    #[inline]
     pub const fn get(self, square: Square) -> bool {
         self.board & (1 << square.0) != 0
     }
     // Sets the square to true
+    #[inline]
     pub const fn set(self, square: Square) -> Self {
         BitBoard::from_u64(self.board | 1 << square.0)
     }
 
     // Sets the square to false
+    #[inline]
     pub const fn clear(self, square: Square) -> Self {
         BitBoard::from_u64(self.board & !(1 << square.0))
     }
 
+    #[inline]
     pub const fn is_empty(self) -> bool {
         self.board == 0
     }
 
+    #[inline]
     pub const fn popcount(self) -> u32 {
         self.board.count_ones()
     }
 
     /// Get a single rank
     /// Ranks are numbered from 0 (1st rank) to 7 (8th rank)
+    #[inline]
     pub const fn rank(self, rank: u8) -> u8 {
         (self.board >> (rank * 8)) as u8
     }
 
+    #[inline]
     pub const fn file(self, file: u8) -> u8 {
         self.rotate().rank(file)
     }
+    #[inline]
     pub const fn rotate(self) -> Self {
         self.flip_vertical().flip_diagonal()
     }
+    #[inline]
     pub const fn rotate_270(self) -> Self {
         self.flip_diagonal().flip_vertical()
     }
+    #[inline]
     pub const fn rotate_45(self) -> Self {
         let k1 = 0xAAAA_AAAA_AAAA_AAAA;
         let k2 = 0xCCCC_CCCC_CCCC_CCCC;
@@ -136,6 +157,7 @@ impl BitBoard {
         x ^= k4 & (x ^ x.rotate_right(32));
         Self::from_u64(x)
     }
+    #[inline]
     pub const fn rotate_315(self) -> Self {
         let k1 = 0x5555_5555_5555_5555;
         let k2 = 0x3333_3333_3333_3333;
@@ -147,6 +169,7 @@ impl BitBoard {
         Self::from_u64(x)
     }
 
+    #[inline]
     pub const fn flip_horizontal(self) -> Self {
         let k1 = 0x5555_5555_5555_5555;
         let k2 = 0x3333_3333_3333_3333;
@@ -157,10 +180,12 @@ impl BitBoard {
         x = ((x >> 4) & k4) + 16 * (x & k4);
         BitBoard::from_u64(x)
     }
+    #[inline]
     pub const fn flip_vertical(self) -> Self {
         BitBoard::from_u64(self.board.swap_bytes())
     }
 
+    #[inline]
     pub const fn flip_diagonal(self) -> Self {
         let k1 = 0x5500_5500_5500_5500;
         let k2 = 0x3333_0000_3333_0000;
@@ -176,6 +201,7 @@ impl BitBoard {
     }
     /// Diagonal_id is rank + file
     /// Assumes board is already rotated
+    #[inline]
     pub fn diagonal(self, diagonal_id: u8) -> u8 {
         let len = if diagonal_id >= 8 {
             15 - diagonal_id
@@ -190,6 +216,7 @@ impl BitBoard {
 
     /// Diagonal_id is rank - file
     /// Assumes board is already rotated
+    #[inline]
     pub fn antidiagonal(self, diagonal_id: i8) -> u8 {
         let len = 8 - diagonal_id.abs(); // Between 1 and 8
         let n = i16::from(diagonal_id);
@@ -197,6 +224,7 @@ impl BitBoard {
         ((self.board >> offset) & !(!0 << len as u64)) as u8
     }
 
+    #[inline]
     pub fn first_piece(self) -> Option<Square> {
         let square = self.board.trailing_zeros();
         if square == 64 {
@@ -220,12 +248,14 @@ impl IntoIterator for BitBoard {
     type Item = Square;
     type IntoIter = BitBoardIterator;
 
+    #[inline]
     fn into_iter(self) -> Self::IntoIter {
         BitBoardIterator::new(self)
     }
 }
 
 impl iter::FromIterator<Square> for BitBoard {
+    #[inline]
     fn from_iter<T: IntoIterator<Item = Square>>(iter: T) -> Self {
         let mut board = Self::empty();
         for square in iter {
@@ -240,6 +270,7 @@ pub struct BitBoardIterator {
 }
 
 impl BitBoardIterator {
+    #[inline]
     const fn new(board: BitBoard) -> Self {
         Self { board }
     }
@@ -247,6 +278,7 @@ impl BitBoardIterator {
 
 impl Iterator for BitBoardIterator {
     type Item = Square;
+    #[inline]
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(next) = self.board.first_piece() {
             self.board = self.board.clear(next);
@@ -265,12 +297,14 @@ pub struct SquaresIterator {
 }
 
 impl SquaresIterator {
+    #[inline]
     pub fn new() -> Self {
         SquaresIterator { index: 0 }
     }
 }
 
 impl Default for SquaresIterator {
+    #[inline]
     fn default() -> Self {
         Self::new()
     }
@@ -278,6 +312,7 @@ impl Default for SquaresIterator {
 
 impl Iterator for SquaresIterator {
     type Item = Square;
+    #[inline]
     fn next(&mut self) -> Option<Square> {
         if self.index >= 64 {
             None
